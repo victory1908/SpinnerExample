243532e367be8663970744852bb7a4867fbe2261
victory1908 <victory1908@gmail.com>
2015-11-19 17:21:40 +0800

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/spinnerexample/Config.java
@@ -0,0 +1,18 @@
+package net.simplifiedcoding.spinnerexample;
+
+/**
+ * Created by Belal on 10/31/2015.
+ */
+public class Config {
+    //JSON URL
+    public static final String DATA_URL = "http://simplifiedcoding.16mb.com/Spinner/json.php";
+
+    //Tags used in the JSON String
+    public static final String TAG_USERNAME = "username";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_COURSE = "course";
+    public static final String TAG_SESSION = "session";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "result";
+}
